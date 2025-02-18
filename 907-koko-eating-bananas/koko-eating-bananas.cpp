42099008ca86783ea79944716a6033f4c40903b0
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-02-19 00:36:40 +0530

    Time: 4 ms (90.34%) | Memory: 22.9 MB (43.02%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long kokoEating(long long n , vector<int>&piles){
+        long long ans = 0;
+        for(auto i : piles){
+            ans += (i + n - 1) / n;
+        }
+        return ans;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long low = 1;
+        long long high = *max_element(piles.begin(),piles.end());
+        long long ans = high;
+        while(low <= high){
+            long long mid = low + (high - low) / 2;
+            long long pred = kokoEating(mid,piles);
+            if(pred <= h){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
