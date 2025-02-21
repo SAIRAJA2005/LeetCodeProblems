5d7a8333c4b3d81b52edd707c78846ccd81e2e0c
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-02-21 16:16:11 +0530

    Time: 43 ms (6.45%) | Memory: 17.5 MB (6.21%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void generator(int i , vector<int>ans, set<vector<int>>&res, vector<int>&nums){
+        if(i >= nums.size()){
+            for (auto it : ans) cout << it << " ";
+            cout << "\n";
+            sort(ans.begin(),ans.end());
+            res.insert(ans);
+            return;
+        }
+        ans.push_back(nums[i]);
+        generator(i+1,ans,res,nums);
+        ans.pop_back();
+        generator(i+1,ans,res,nums);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>>res;
+        vector<int>ans;
+        generator(0,ans,res,nums);
+        vector<vector<int>>resOne;
+        for(auto i : res){
+            resOne.push_back(i);
+        }
+        return resOne;
+    }
+};
