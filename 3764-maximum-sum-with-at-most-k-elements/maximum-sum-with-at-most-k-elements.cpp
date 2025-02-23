05c274acd40367f56f500bec255014f017625a64
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-02-23 10:15:24 +0530

    Time: 125 ms (94.74%) | Memory: 145.6 MB (10.53%) - LeetSync

---
+++ 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<long long > findMaxNum(vector<int>&gr , int k){
+        sort(gr.begin(),gr.end());
+        vector<long long>res;
+        for(int i = gr.size() - 1 ; i >= 0 ; i--){
+            if(k == res.size()) break;
+            else{
+                res.push_back(gr[i]);
+            }
+        }
+        return res;
+    }
+    long long maxSum(vector<vector<int>>& grid, vector<int>& limits, int k) {
+        vector<long long>res;
+        for(int i = 0 ; i < grid.size() ; i++){
+            vector<long long> s = findMaxNum(grid[i], limits[i]);
+            for(auto i : s){
+                res.push_back(i);
+            }
+        }
+        sort(res.begin(),res.end());
+        long long sm = 0;
+        int cnt = 0;
+        for(int i = res.size() - 1 ; i >=0 ; i--){
+            if(k == 0) break;
+            sm += res[i];
+            k--;
+        }
+        return sm;
+    }
+};
