4212c1b5b3d447ad5f4f8af290935f3785f4ccc2
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-02-25 23:34:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (63.33%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void generatePara(int close, int open, string s, vector<string>&res, int n){
+        if(close == open && (open + close) == 2 * n){
+            res.push_back(s);
+            return;
+        }
+
+        if(open < n){
+            generatePara(close,open+1,s+'(',res,n);
+        }
+
+        if(close < open){
+            generatePara(close+1,open,s+')',res,n);
+        }
+
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>res;
+        string s;
+        generatePara(0,0,s,res,n);
+        return res;
+    }
+};
