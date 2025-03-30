21804e924ce3ff77f7893f4d4c8cefba7931196a
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-03-30 10:26:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (57.89%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        string ful = "zyxwvutsrqponmlkjihgfedcba";
+        int pro = 1;
+        for(int i = 0 ; i < s.size() ; i++){
+            int ind = ful.find(s[i]);
+            pro += (ind + 1) * (i + 1);
+        }
+        return pro - 1;
+    }
+};
