562347178bfb6b340792d4a77c9f1f5aa0f0aa10
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-06-03 23:03:51 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/version/cpp/55-JumpGame_20250603_230351.cpp
@@ -0,0 +1,22 @@
+// Last updated: 03/06/2025, 23:03:51
+// Jump Game
+class Solution {
+public:
+    bool jumping(int i , vector<int>&nums , int n, vector<int>&dp){
+        if(i >= n - 1) return true;
+        if(nums[i] == 0) return false;
+        if(dp[i] != -1) return dp[i];
+        for(int j = 1 ; j <= nums[i] ; j++){
+            if(jumping(i + j , nums, n, dp)){
+                dp[i] = 1;
+                return true;
+            }
+        }
+        dp[i] = 0;
+        return false;
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return jumping(0 , nums , nums.size(), dp);
+    }
+};
