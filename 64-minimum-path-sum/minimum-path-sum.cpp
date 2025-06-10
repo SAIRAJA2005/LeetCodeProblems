63d57fe89d7c9587c09afc9df0f0a3cc2b9696a7
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-06-10 23:33:30 +0530

    Time: 2 ms (35.59%) | Memory: 16.5 MB (5.01%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+
     int findThePath(int i , int j , vector<vector<int>>&grid , vector<vector<int>>&dp){
         if(i == 0 && j == 0) return grid[i][j];
         if(i < 0 || j < 0) return 1e9;
@@ -8,8 +9,10 @@ public:
         int left = grid[i][j] + findThePath(i , j - 1 , grid , dp);
         return dp[i][j] = min(up , left);
     }
+
     int minPathSum(vector<vector<int>>& grid) {
         vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),-1));
         return findThePath(grid.size() - 1, grid[0].size() - 1 , grid , dp);
     }
+    
 };
