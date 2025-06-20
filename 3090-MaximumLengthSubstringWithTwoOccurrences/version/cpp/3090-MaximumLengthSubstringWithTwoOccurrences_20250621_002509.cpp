6c2c6dc8c0f4c71d8c0d73f7cb3c4661d810d1a3
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-06-21 00:25:11 +0530

    Create 3090-MaximumLengthSubstringWithTwoOccurrences

---
+++ 3090-MaximumLengthSubstringWithTwoOccurrences/version/cpp/3090-MaximumLengthSubstringWithTwoOccurrences_20250621_002509.cpp
@@ -0,0 +1,18 @@
+// Last updated: 21/06/2025, 00:25:09
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        int mx = 0;
+        map<char,int>mp;
+        int i = 0;
+        for(int j = 0 ; j < s.size() ; j++){
+            mp[s[j]]++;
+            while(mp[s[j]] > 2){
+                mp[s[i]]--;
+                i++;
+            }
+            mx = max(mx,j - i + 1);
+        }
+        return mx;
+    }
+};
