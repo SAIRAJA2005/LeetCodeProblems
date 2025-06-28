9d3e061b7127e2dffd1b44d2c687204a23273296
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-06-29 00:29:11 +0530

    Create 2099-FindSubsequenceofLengthKWiththeLargestSum

---
+++ 2099-FindSubsequenceofLengthKWiththeLargestSum/version/cpp/2099-FindSubsequenceofLengthKWiththeLargestSum_20250629_002911.cpp
@@ -0,0 +1,23 @@
+// Last updated: 29/06/2025, 00:29:11
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<pair<int, int>> arr;
+        for (int i = 0; i < n; ++i) {
+            arr.push_back({nums[i], i});
+        }
+        sort(arr.begin(), arr.end(), [](auto &a, auto &b) {
+            return a.first > b.first;
+        });
+        sort(arr.begin(), arr.begin() + k, [](auto &a, auto &b) {
+            return a.second < b.second;
+        });
+        vector<int> res;
+        for (int i = 0; i < k; ++i) {
+            res.push_back(arr[i].first);
+        }
+        return res;
+
+    }
+};
