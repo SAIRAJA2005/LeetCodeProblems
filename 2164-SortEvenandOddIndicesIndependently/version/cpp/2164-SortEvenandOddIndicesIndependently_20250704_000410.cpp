cd7b3162973bcff7205eb98b528527047b197460
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-07-04 00:04:13 +0530

    Create 2164-SortEvenandOddIndicesIndependently

---
+++ 2164-SortEvenandOddIndicesIndependently/version/cpp/2164-SortEvenandOddIndicesIndependently_20250704_000410.cpp
@@ -0,0 +1,25 @@
+// Last updated: 04/07/2025, 00:04:10
+class Solution {
+public:
+    vector<int> sortEvenOdd(vector<int>& nums) {
+        vector<int>e;
+        vector<int>o;
+
+        for(int i = 0 ; i < nums.size() ;i++){
+            if(i % 2 == 0) e.push_back(nums[i]);
+            else o.push_back(nums[i]);
+        }
+
+        sort(e.begin() , e.end());
+        sort(o.begin() , o.end() , greater<int>());
+
+        int eidx = 0  , oidx = 0;
+
+        for(int i = 0 ; i  < nums.size() ;i++){
+            if(i % 2 == 0) nums[i] = e[eidx++];
+            else nums[i] = o[oidx++];
+        }
+
+        return nums;       
+    }
+};
