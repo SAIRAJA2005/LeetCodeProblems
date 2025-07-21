8d90766a59bd34d304318105c7733ba227165bc2
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-07-21 23:54:39 +0530

    Create 1957-DeleteCharacterstoMakeFancyString

---
+++ 1957-DeleteCharacterstoMakeFancyString/version/cpp/1957-DeleteCharacterstoMakeFancyString_20250721_235436.cpp
@@ -0,0 +1,18 @@
+// Last updated: 21/07/2025, 23:54:36
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string ans = "";
+        if (s.size() == 1) return s;
+        ans.push_back(s[0]);
+        ans.push_back(s[1]);
+        for (int i = 2; i < s.size(); i++) {
+            if (s[i] == s[i - 1] && s[i] == s[i - 2]) {
+                continue;
+            } else {
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
