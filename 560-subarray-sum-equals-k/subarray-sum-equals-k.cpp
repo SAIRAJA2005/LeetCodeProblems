4b814bb6381f746b1771d509d9f59b00e383a3c4
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-07-30 13:10:37 +0530

    Time: 40 ms (72.80%) | Memory: 45.3 MB (68.78%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int>preFreq;
+        int cnt = 0, sm = 0;
+        preFreq[0] = 1;
+        for(auto num : nums){
+            sm += num;
+            if(preFreq.find(sm - k) != preFreq.end()){
+                cnt += preFreq[sm - k];
+            }
+            preFreq[sm]++;
+        }
+        return cnt;
+    }
+};
