19ce81df1a4cbbe58af6566685270e498424bc28
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-08-13 17:53:13 +0530

    Create 912-SortanArray

---
+++ 912-SortanArray/version/java/912-SortanArray_20250813_175311.java
@@ -0,0 +1,42 @@
+// Last updated: 13/08/2025, 17:53:11
+class Solution {
+    public void merge(int[] nums, int low, int mid, int high){
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+        int[] arr = new int[high - low + 1];
+        while(i <= mid && j <= high){
+            if(nums[j] > nums[i]){
+                arr[k++] = nums[i++];
+            }
+            else{
+                arr[k++] = nums[j++];
+            }
+        }
+
+        while(i <= mid){
+            arr[k++] = nums[i++];
+        }
+
+        while(j <= high){
+            arr[k++] = nums[j++];
+        }
+
+        for(int q = 0 ; q < high - low + 1 ; q++){
+            nums[q + low] = arr[q];
+        }
+    }
+    public void mergeSort(int[] nums, int low, int high){
+        if(low < high){
+            int mid = (low + high) / 2;
+            mergeSort(nums, low, mid);
+            mergeSort(nums, mid + 1, high);
+            merge(nums, low, mid, high);
+        }
+    }
+    public int[] sortArray(int[] nums) {
+        int n = nums.length;
+        mergeSort(nums, 0, n - 1);
+        return nums;
+    }
+}
