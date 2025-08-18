2ce2d6c8b57ed2620814d7f5ad3a80f0e9dba820
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-08-18 21:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (35.41%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void callMeBro(vector<vector<int>>&mat, int i , int j){
+        for(int start = i ; start < mat[0].size() ; start++){
+            swap(mat[start][j], mat[j][start]);
+        }
+    }
+    void rotate(vector<vector<int>>& matrix) {
+        int i = 0;
+        int j = 0;
+        while(i <= j && j < matrix.size()){
+            callMeBro(matrix, i , j);
+            i++;
+            j++;
+        }
+        for(i = 0 ; i < matrix.size() ; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
