74f48e7d7a55f45a7b3c41361373a26cb7dbbd10
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-08-22 22:33:35 +0530

    Create 79-WordSearch

---
+++ 79-WordSearch/version/java/79-WordSearch_20250822_223334.java
@@ -0,0 +1,26 @@
+// Last updated: 22/08/2025, 22:33:34
+class Solution {
+    public boolean wordSearch(char[][] board, String word, int ind, int row, int col){
+        if(ind == word.length()) return true;
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) return false;
+        if (board[row][col] != word.charAt(ind)) return false;
+        char temp = board[row][col];
+        board[row][col] = '#';
+        boolean found = wordSearch(board, word, ind + 1, row + 1, col) ||
+                        wordSearch(board, word, ind + 1, row - 1, col) ||
+                        wordSearch(board, word, ind + 1, row, col + 1) ||
+                        wordSearch(board, word, ind + 1, row, col - 1);
+        board[row][col] = temp;
+        return found;
+    }
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (wordSearch(board, word, 0, i, j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
