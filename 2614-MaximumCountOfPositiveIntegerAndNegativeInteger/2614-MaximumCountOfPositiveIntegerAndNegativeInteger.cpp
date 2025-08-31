dc581c176b1f22660e448dbf8253c5a16991ad4f
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 00:59:22 +0530

    Create 2614-MaximumCountOfPositiveIntegerAndNegativeInteger

---
+++ 2614-MaximumCountOfPositiveIntegerAndNegativeInteger/2614-MaximumCountOfPositiveIntegerAndNegativeInteger.cpp
@@ -0,0 +1,11 @@
+// Last updated: 01/09/2025, 00:59:19
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0; 
+        int firstPositive = lower_bound(nums.begin(), nums.end(), 1) - nums.begin();
+        int lastNegative = lower_bound(nums.begin(), nums.end(), 0) - nums.begin();
+        return max(lastNegative , n - firstPositive);
+    }
+};
