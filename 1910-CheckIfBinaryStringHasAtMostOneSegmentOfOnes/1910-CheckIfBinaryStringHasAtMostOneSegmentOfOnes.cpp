98df33e2a57f1c38ae30084f72f2c989519d2fdd
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:01:08 +0530

    Create 1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes

---
+++ 1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes/1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes.cpp
@@ -0,0 +1,10 @@
+// Last updated: 01/09/2025, 01:01:05
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        for(int i = 1 ; i < s.size() ; i++){
+            if(s[i - 1] == '0' && s[i] == '1') return false;
+        }
+        return true;
+    }
+};
