08d2e0cca2b4fc511085a91efa542a8f15e82ec8
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:06:54 +0530

    Create 15-3sum

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,34 @@
+// Last updated: 01/09/2025, 01:06:51
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int target = 0;
+        sort(nums.begin() , nums.end());
+        set<vector<int>>ans;
+        int n = nums.size();
+        for(int i = 0 ; i < n ; i++){
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k){
+                int sm = nums[i] + nums[j] + nums[k];
+                if(sm == target) {
+                    ans.insert({nums[i], nums[j],nums[k]});
+                    j++;
+                    k--;
+                }
+                else if(sm < target){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+
+        }
+        vector<vector<int>>res;
+        for(auto triple : ans){
+            res.push_back(triple);
+        }
+        return res;
+    }
+};
