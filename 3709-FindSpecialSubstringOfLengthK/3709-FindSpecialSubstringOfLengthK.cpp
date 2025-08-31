c1a63ea24fecf47acd5a1bc135f0d532227f8a29
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 00:57:43 +0530

    Create 3709-FindSpecialSubstringOfLengthK

---
+++ 3709-FindSpecialSubstringOfLengthK/3709-FindSpecialSubstringOfLengthK.cpp
@@ -0,0 +1,19 @@
+// Last updated: 01/09/2025, 00:57:40
+class Solution {
+public:
+    bool hasSpecialSubstring(string s, int k) {
+        if(s.size() == 1) return true;
+        int cnt = 1;
+        for(int i = 1 ; i < s.size() ; i++){
+            cnt = 1;
+            while(s[i-1] == s[i]){
+                cnt++;
+                i++;
+            }
+            //cout << cnt << " ";
+            if(cnt == k) return true;
+        }
+        if(k == 1 && s[s.size()-1] != s[s.size()-2]) return true;
+        return false;
+    }
+};
