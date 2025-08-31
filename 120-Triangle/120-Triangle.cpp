1c925b7145b789e91b4e66d803c07a6082a10de4
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:05:46 +0530

    Create 120-Triangle

---
+++ 120-Triangle/120-Triangle.cpp
@@ -0,0 +1,15 @@
+// Last updated: 01/09/2025, 01:05:43
+class Solution {
+public:
+    int findTriangle(int i , int j , vector<vector<int>>&triangle,vector<vector<int>>&dp){
+        if(i == triangle.size() - 1) return triangle[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int one = triangle[i][j] + findTriangle(i + 1 , j , triangle ,dp);
+        int two = triangle[i][j] + findTriangle(i + 1 , j + 1 , triangle , dp);
+        return dp[i][j] = min(one , two);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<vector<int>>dp(triangle.size(),vector<int>(triangle.size(),-1));
+        return findTriangle(0,0,triangle,dp);
+    }
+};
