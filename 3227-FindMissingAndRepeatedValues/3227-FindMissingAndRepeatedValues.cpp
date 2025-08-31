1f544e4a81d3e33ecb42073b96d6382c35b01aba
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 00:58:25 +0530

    Create 3227-FindMissingAndRepeatedValues

---
+++ 3227-FindMissingAndRepeatedValues/3227-FindMissingAndRepeatedValues.cpp
@@ -0,0 +1,27 @@
+// Last updated: 01/09/2025, 00:58:22
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        vector<int>res;
+        int sm = 0;
+        unordered_map<int,int>mp;
+        for(auto i : grid){
+            for(auto j : i){
+                mp[j]++;
+                sm += j; // 8
+            }
+        }
+        for(auto i : mp){
+            if(i.second > 1){
+                res.push_back(i.first);
+                break;
+            }
+        }
+        sm = sm - res[0];
+        int n = grid.size();
+        n = n * n;
+        int k = (n * (n + 1)) / 2;    // 10
+        res.push_back(k - sm);
+        return res;
+    }
+};
