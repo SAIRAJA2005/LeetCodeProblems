660853bcb40d32a1442904ccb5a8ebd606eeea04
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:02:44 +0530

    Create 1112-FindWordsThatCanBeFormedByCharacters

---
+++ 1112-FindWordsThatCanBeFormedByCharacters/1112-FindWordsThatCanBeFormedByCharacters.java
@@ -0,0 +1,28 @@
+// Last updated: 01/09/2025, 01:02:41
+class Solution {
+    public int countCharacters(String[] words, String chars) {
+        int[] charCount = new int[26];
+        for (char ch : chars.toCharArray()) {
+            charCount[ch - 'a']++;
+        }
+        int cnt = 0;
+        for (String word : words) {
+            int[] wordCount = new int[26];
+            for (char ch : word.toCharArray()) {
+                wordCount[ch - 'a']++;
+            }
+            boolean canForm = true;
+            for (int i = 0; i < 26; i++) {
+                if (wordCount[i] > charCount[i]) {
+                    canForm = false;
+                    break;
+                }
+            }
+
+            if (canForm) {
+                cnt += word.length();
+            }
+        }
+        return cnt;
+    }
+}
