af7a216d130023977849bc9de1cda56f8e919a07
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:06:34 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.cpp
@@ -0,0 +1,21 @@
+// Last updated: 01/09/2025, 01:06:31
+class Solution {
+public:
+    int jumping(int i, vector<int>& nums, vector<int>&dp) {
+        int n = nums.size();
+        if (i >= n - 1) return 0;
+        if (nums[i] == 0) return 1e9;
+        if(dp[i] != -1) return dp[i];
+        int minJumps = 1e9;
+        for (int j = 1; j <= nums[i]; j++) {
+            minJumps = min(minJumps, 1 + jumping(i + j, nums, dp));
+        }
+
+        return dp[i] = minJumps;
+    }
+
+    int jump(vector<int>& nums) {
+        vector<int>dp(nums.size(), -1);
+        return jumping(0 , nums , dp);
+    }
+};
