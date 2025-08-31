5227639d276b69202bf1f9015729239df0987df9
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:03:09 +0530

    Create 907-KokoEatingBananas

---
+++ 907-KokoEatingBananas/907-KokoEatingBananas.cpp
@@ -0,0 +1,28 @@
+// Last updated: 01/09/2025, 01:03:06
+class Solution {
+public:
+    long long kokoEating(long long n , vector<int>&piles){
+        long long ans = 0;
+        for(auto i : piles){
+            ans += ceil(double(i) / double(n));
+        }
+        return ans;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long low = 1;
+        long long high = *max_element(piles.begin(),piles.end());
+        long long ans = high;
+        while(low <= high){
+            long long mid = low + (high - low) / 2;
+            long long pred = kokoEating(mid,piles);
+            if(pred <= h){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
