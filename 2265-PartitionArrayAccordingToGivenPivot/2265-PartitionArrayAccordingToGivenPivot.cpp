82f222fd7c779352bacc53478fa18fbbb13f77b7
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:00:06 +0530

    Create 2265-PartitionArrayAccordingToGivenPivot

---
+++ 2265-PartitionArrayAccordingToGivenPivot/2265-PartitionArrayAccordingToGivenPivot.cpp
@@ -0,0 +1,21 @@
+// Last updated: 01/09/2025, 01:00:03
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int>arr1;
+        vector<int>arr2;
+        vector<int>arr3;
+        for(auto i : nums){
+            if(i == pivot) arr2.push_back(i);
+            else if(i < pivot) arr1.push_back(i);
+            else arr3.push_back(i);
+        }
+        for(int i = 0 ; i < arr2.size() ; i++){
+            arr1.push_back(arr2[i]);
+        }
+        for(int i = 0 ; i < arr3.size() ; i++){
+            arr1.push_back(arr3[i]);
+        }
+        return arr1;
+    }
+};
