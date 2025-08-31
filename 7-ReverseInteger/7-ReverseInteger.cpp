f5b3aaeb1fa03fbe54b8ab476e937e2a66ef3ab8
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:07:01 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -0,0 +1,14 @@
+// Last updated: 01/09/2025, 01:06:58
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while(x != 0){
+            int rem = x % 10;
+            if ((rev > INT_MAX / 10) || (rev < INT_MIN / 10)) return 0;
+            rev = (rev * 10) + rem;
+            x /= 10;
+        }
+        return rev;
+    }
+};
