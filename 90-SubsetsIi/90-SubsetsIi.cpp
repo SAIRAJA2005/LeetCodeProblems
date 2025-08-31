dc811437cf40674bd0e81ea9e47503b3ab3738d8
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:06:04 +0530

    Create 90-SubsetsIi

---
+++ 90-SubsetsIi/90-SubsetsIi.cpp
@@ -0,0 +1,27 @@
+// Last updated: 01/09/2025, 01:06:01
+class Solution {
+public:
+    void generator(int i , vector<int>ans, set<vector<int>>&res, vector<int>&nums){
+        if(i >= nums.size()){
+            for (auto it : ans) cout << it << " ";
+            cout << "\n";
+            sort(ans.begin(),ans.end());
+            res.insert(ans);
+            return;
+        }
+        generator(i+1,ans,res,nums);
+        ans.push_back(nums[i]);
+        //ans.pop_back();
+        generator(i+1,ans,res,nums);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>>res;
+        vector<int>ans;
+        generator(0,ans,res,nums);
+        vector<vector<int>>resOne;
+        for(auto i : res){
+            resOne.push_back(i);
+        }
+        return resOne;
+    }
+};
