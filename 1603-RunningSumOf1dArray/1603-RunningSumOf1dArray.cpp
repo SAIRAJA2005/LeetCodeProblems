8b89000889d349d4e762e062aee05657dcab526d
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:01:47 +0530

    Create 1603-RunningSumOf1dArray

---
+++ 1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 01/09/2025, 01:01:44
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int i,k=0;
+        vector<int>num;
+        for(i=0;i<nums.size();i++){
+            if(nums[i]>=-1000000 && nums[i]<=1000000){
+                k+=nums[i];
+                num.push_back(k);
+            }
+        }
+        return num;
+    }
+};
