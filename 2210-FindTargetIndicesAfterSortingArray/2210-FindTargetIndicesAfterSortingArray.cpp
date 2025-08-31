5c238e5fff906b7dc8cf2106b22c5c46fbaff87a
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:00:19 +0530

    Create 2210-FindTargetIndicesAfterSortingArray

---
+++ 2210-FindTargetIndicesAfterSortingArray/2210-FindTargetIndicesAfterSortingArray.cpp
@@ -0,0 +1,14 @@
+// Last updated: 01/09/2025, 01:00:16
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<int>vec;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] == target){
+                vec.push_back(i);
+            }
+        }
+        return vec;
+    }
+};
