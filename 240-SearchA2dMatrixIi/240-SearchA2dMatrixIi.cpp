da907b16a5ce94e7a0ab90f504439e287c6b099d
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:04:48 +0530

    Create 240-SearchA2dMatrixIi

---
+++ 240-SearchA2dMatrixIi/240-SearchA2dMatrixIi.cpp
@@ -0,0 +1,21 @@
+// Last updated: 01/09/2025, 01:04:45
+class Solution {
+public:
+    bool binarySearchRow(vector<int>& row, int target) {
+        int low = 0, high = row.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (row[mid] == target) return true;
+            else if (row[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for (auto& row : matrix) {
+            if (binarySearchRow(row, target)) return true;
+        }
+        return false;
+    }
+};
