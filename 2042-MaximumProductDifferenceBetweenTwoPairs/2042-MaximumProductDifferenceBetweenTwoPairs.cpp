4d13edc1b7ada7ec88c9f686df9eeac7e60ec51f
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:00:45 +0530

    Create 2042-MaximumProductDifferenceBetweenTwoPairs

---
+++ 2042-MaximumProductDifferenceBetweenTwoPairs/2042-MaximumProductDifferenceBetweenTwoPairs.cpp
@@ -0,0 +1,9 @@
+// Last updated: 01/09/2025, 01:00:43
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int k = nums.size();
+        return (nums[k-1]*nums[k-2])-(nums[0]*nums[1]);
+    }
+};
