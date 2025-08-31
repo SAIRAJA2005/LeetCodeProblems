a14b2635022e4ab005bd6a008db693e61492a546
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:01:34 +0530

    Create 1651-ShuffleString

---
+++ 1651-ShuffleString/1651-ShuffleString.cpp
@@ -0,0 +1,15 @@
+// Last updated: 01/09/2025, 01:01:31
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string NewString = "";
+        map<int,char>mp;
+        for(int i = 0 ; i < indices.size() ; i++){
+            mp[indices[i]] = s[i];
+        }
+        for(auto i : mp){
+            NewString += i.second;
+        }
+        return NewString;
+    }
+};
