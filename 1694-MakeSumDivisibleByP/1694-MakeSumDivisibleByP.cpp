d0fa13902e6d771bd76866ef81c8453da328c200
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:01:29 +0530

    Create 1694-MakeSumDivisibleByP

---
+++ 1694-MakeSumDivisibleByP/1694-MakeSumDivisibleByP.cpp
@@ -0,0 +1,29 @@
+// Last updated: 01/09/2025, 01:01:26
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        int n = nums.size();
+        int SUM = 0;
+        for(int &num : nums) {
+            SUM = (SUM + num) % p;
+        }
+        int target = SUM%p;
+        if(target == 0) {
+            return 0;
+        }
+        unordered_map<int, int> mp; 
+        int curr = 0;
+        mp[0] = -1;
+        int result = n;
+        for(int j = 0; j < n; j++) {
+            curr = (curr + nums[j]) % p;
+            int remain = (curr - target + p) % p;
+            if(mp.find(remain) != mp.end()) {
+                result = min(result, j - mp[remain]);
+            }
+            mp[curr] = j;
+        }
+
+        return result == n ? -1 : result;
+    }
+};
