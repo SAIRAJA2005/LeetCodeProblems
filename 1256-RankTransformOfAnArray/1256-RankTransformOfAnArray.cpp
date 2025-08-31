50e468bf9e1590256773a2f6f68149a1188a1fb5
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:02:32 +0530

    Create 1256-RankTransformOfAnArray

---
+++ 1256-RankTransformOfAnArray/1256-RankTransformOfAnArray.cpp
@@ -0,0 +1,25 @@
+// Last updated: 01/09/2025, 01:02:29
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        if (arr.empty()) {
+            return {};
+        }
+        vector<int> dup = arr;
+        sort(dup.begin(), dup.end());
+        map<int, int> mp;
+        mp[dup[0]] = 1;
+        for (int i = 1; i < dup.size(); i++) {
+            if (dup[i] > dup[i - 1]) {
+                mp[dup[i]] = mp[dup[i - 1]] + 1;
+            } else {
+                mp[dup[i]] = mp[dup[i - 1]];
+            }
+        }
+        vector<int> res;
+        for (int x : arr) {
+            res.push_back(mp[x]);
+        }
+        return res;
+    }
+};
