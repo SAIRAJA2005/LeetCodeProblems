5675a2e721ca9fe0f65293055f7a642df53c1d43
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:00:56 +0530

    Create 1965-SumOfDigitsInBaseK

---
+++ 1965-SumOfDigitsInBaseK/1965-SumOfDigitsInBaseK.cpp
@@ -0,0 +1,21 @@
+// Last updated: 01/09/2025, 01:00:53
+class Solution {
+public:
+    int sumBase(int n, int k) {
+        int num = 0, i=0, ans=0;
+        while(n)   
+        {
+            int rem = n%k;
+            num = rem * pow(10,i) + num;
+            n=n/k;
+            i++;
+        }
+        while(num) 
+        {
+            int digit = num%10;
+            ans = ans + digit;
+            num = num/10;
+        }
+        return ans;
+    }
+};
