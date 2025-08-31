50a415ed3a2a5935ea02e7bb523e76d6b16d78ad
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:06:28 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -0,0 +1,14 @@
+// Last updated: 01/09/2025, 01:06:25
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int sum = 0;
+        int maxiSum = Integer.MIN_VALUE;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(sum < 0) sum = 0;
+            sum += nums[i];
+            maxiSum = Math.max(maxiSum, sum);
+        }
+        return maxiSum;
+    }
+}
