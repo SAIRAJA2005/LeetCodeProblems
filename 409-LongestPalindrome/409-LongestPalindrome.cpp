b31125835e7fc7cc2680f2c2ce7b5bafa8dc2407
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:04:17 +0530

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.cpp
@@ -0,0 +1,26 @@
+// Last updated: 01/09/2025, 01:04:14
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        string s1 = s;
+        reverse(s1.begin(),s1.end());
+        if(s1 == s) return s.size();
+        map<char,int>mp;
+        for(char i : s){
+            mp[i]++;
+        }
+        bool yes = false;
+        int cnt = 0;
+        for(auto ch : mp){
+            if(ch.second % 2 == 0){
+                cnt += ch.second;
+            }
+            else{
+                cnt += ch.second-1;
+                yes = true;
+            }
+        }
+        if(yes) return cnt+1;
+        return cnt;
+    }
+};
