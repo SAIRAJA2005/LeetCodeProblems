6304ef6e90c5bd6418991354611c3b6a9a5b3c15
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:06:41 +0530

    Create 40-CombinationSumIi

---
+++ 40-CombinationSumIi/40-CombinationSumIi.cpp
@@ -0,0 +1,24 @@
+// Last updated: 01/09/2025, 01:06:38
+class Solution {
+public:
+    void combinationSum(int idx , vector<int>&candidates , int tar, vector<vector<int>>&res, vector<int>&ans){
+        if(tar == 0){
+            res.push_back(ans);
+            return;
+        }
+        for(int i = idx ; i < candidates.size() ; i++){
+            if(i > idx && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > tar) break;
+            ans.push_back(candidates[i]);
+            combinationSum(i+1,candidates,tar - candidates[i],res,ans);
+            ans.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>res;
+        vector<int>ans;
+        combinationSum(0,candidates,target,res,ans);
+        return res;
+    }
+};
