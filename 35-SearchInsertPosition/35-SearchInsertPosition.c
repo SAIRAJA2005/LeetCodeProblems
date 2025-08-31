24f62f48fb72f1832fbf4045e2461e4680466b35
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:06:45 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.c
@@ -0,0 +1,13 @@
+// Last updated: 01/09/2025, 01:06:42
+int searchInsert(int* nums, int numsSize, int target){
+int low=0,high=numsSize-1,mid;
+
+while(low<=high)
+{
+    mid=(low+high)/2;
+    if(target==nums[mid]) return mid;
+    else if (target>nums[mid]) low=mid+1;
+    else  high=mid-1;
+}
+return low;
+}
