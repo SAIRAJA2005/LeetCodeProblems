e4a078a25147ad433a1c8541956f2246762ea2a2
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 00:58:56 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 01/09/2025, 00:58:53
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
