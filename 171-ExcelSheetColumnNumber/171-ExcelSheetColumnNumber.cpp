653fd7a64d30d71f9c9f694c0ab840a7f1fcc391
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:05:28 +0530

    Create 171-ExcelSheetColumnNumber

---
+++ 171-ExcelSheetColumnNumber/171-ExcelSheetColumnNumber.cpp
@@ -0,0 +1,11 @@
+// Last updated: 01/09/2025, 01:05:25
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int result = 0;
+        for (char c : columnTitle) {
+            result = result * 26 + (c - 'A' + 1);
+        }
+        return result;
+    }
+};
