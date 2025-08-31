34635055f6449ed46c6f1ab0225e95ded3b2834b
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:05:18 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -0,0 +1,15 @@
+// Last updated: 01/09/2025, 01:05:15
+class Solution {
+public:
+    int findMax(int i,vector<int>&arr,vector<int>&dp){
+        if(i >= arr.size()) return 0;
+        if(dp[i] != -1) return dp[i];
+        int one = arr[i] + findMax(i+2,arr,dp);
+        int two = findMax(i+1,arr,dp);
+        return dp[i] = max(one,two);
+    }
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return findMax(0,nums,dp);
+    }
+};
