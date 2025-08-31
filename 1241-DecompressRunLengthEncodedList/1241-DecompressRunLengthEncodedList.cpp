d74469ae985c2937a635c78dfe68e04a8cc31d10
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:02:33 +0530

    Create 1241-DecompressRunLengthEncodedList

---
+++ 1241-DecompressRunLengthEncodedList/1241-DecompressRunLengthEncodedList.cpp
@@ -0,0 +1,13 @@
+// Last updated: 01/09/2025, 01:02:30
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i=i+2){
+            for(int j=0;j<nums[i];j++){
+                ans.push_back(nums[i+1]);
+            }
+        }
+        return ans;
+    }
+};
