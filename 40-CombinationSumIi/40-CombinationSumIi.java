12bbf55b413726521dcb24043661c60457222cc7
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:06:37 +0530

    Create 40-CombinationSumIi

---
+++ 40-CombinationSumIi/40-CombinationSumIi.java
@@ -0,0 +1,23 @@
+// Last updated: 01/09/2025, 01:06:34
+class Solution {
+    public void creating(int[] candidates, int target, int sum, int ind, List<List<Integer>>res, List<Integer>lst){
+        if(sum == target){
+            res.add(new ArrayList<>(lst));
+            return;
+        }
+        for(int i = ind ; i < candidates.length ; i++){
+            if(i > ind && candidates[i] == candidates[i - 1]) continue;
+            if(sum + candidates[i] > target) break;
+            lst.add(candidates[i]);
+            creating(candidates, target, sum + candidates[i], i + 1, res, lst);
+            lst.remove(lst.size() - 1);
+        }
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>>res = new ArrayList<>();
+        List<Integer>lst = new ArrayList<>();
+        creating(candidates, target, 0 , 0 , res, lst);
+        return res;
+    }
+}
