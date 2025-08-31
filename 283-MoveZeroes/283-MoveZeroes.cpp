717f24a736735175e8193878d285c3ea18badd0c
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:04:41 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,19 @@
+// Last updated: 01/09/2025, 01:04:38
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int start = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] == 0 && nums[start] == 0){
+                continue;
+            }
+            if(nums[i] == 0){
+                start = i;
+            }
+            if(nums[i] != 0 && nums[start] == 0){
+                swap(nums[i], nums[start]);
+                start++;
+            }
+        }
+    }
+};
