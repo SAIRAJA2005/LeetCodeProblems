8cab48e9aa60b899365ae95df85cf5fca19d2fd0
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:00:50 +0530

    Create 2006-FindTheStudentThatWillReplaceTheChalk

---
+++ 2006-FindTheStudentThatWillReplaceTheChalk/2006-FindTheStudentThatWillReplaceTheChalk.cpp
@@ -0,0 +1,16 @@
+// Last updated: 01/09/2025, 01:00:47
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        int n = chalk.size();
+        long long sum = accumulate(chalk.begin(), chalk.end(), 0LL); 
+        int t = k % sum; 
+        for (int i = 0; i < n; ++i) {
+            if (t < chalk[i]) {
+                return i; 
+            }
+            t -= chalk[i]; 
+        }
+        return -1; 
+    }
+};
