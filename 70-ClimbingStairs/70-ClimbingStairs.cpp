21b8dd59ba58154aad373ab98c40580ded231d41
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:06:14 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,35 @@
+// Last updated: 01/09/2025, 01:06:11
+class Solution {
+public:
+    /* Memoiation */
+    // int dpFun(int n ,vector<int>&dp){
+    //     if(n == 0 || n == 1) return 1;
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+    //     return dp[n] = dpFun(n-1,dp) + dpFun(n-2,dp);
+    // }
+    int climbStairs(int n) {
+
+        /*Tabulation*/
+        
+        // vector<int>dp(n + 1);
+        // /*dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i = 2 ; i <= n ; i++){
+        //     dp[i] = dp[i - 1] + dp[i - 2];
+        // }
+        // return dp[n];
+
+        /*Space Optimization*/
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr = 0;
+        for(int i = 2 ; i <= n ; i++){
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
