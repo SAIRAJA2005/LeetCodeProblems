5992f21dca7d3218831a9a5a56f0bbc265c17da1
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:04:21 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,10 @@
+// Last updated: 01/09/2025, 01:04:18
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<char,int>mp;
+        for(int i=0;i<s.size();i++) mp[s[i]]++;
+        for(int i=0;i<s.size();i++) if(mp[s[i]]==1) return i;
+        return -1;
+    }
+};
