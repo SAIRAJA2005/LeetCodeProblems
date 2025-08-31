448e2b5c60fca42b42b70ee0c38603883c95877c
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-01 01:01:18 +0530

    Create 1818-MaximumScoreFromRemovingSubstrings

---
+++ 1818-MaximumScoreFromRemovingSubstrings/1818-MaximumScoreFromRemovingSubstrings.cpp
@@ -0,0 +1,53 @@
+// Last updated: 01/09/2025, 01:01:15
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        stack<char>st;
+        int sum = 0;
+        if(x < y){
+            for(auto i : s){
+                if(!st.empty() && st.top() == 'b' && i == 'a'){
+                    st.pop();
+                    sum += y;
+                }
+                else st.push(i);
+            }
+            string m ;
+            while(!st.empty()){
+                m.push_back(st.top());
+                st.pop();
+            }
+            reverse(m.begin(),m.end());
+            for(auto i : m){
+                if(!st.empty() && st.top() == 'a' && i == 'b'){
+                    st.pop();
+                    sum += x;
+                }
+                else st.push(i);
+            }
+        }
+        else{
+            for(auto i : s){
+                if(!st.empty() && st.top() == 'a' && i == 'b'){
+                    st.pop();
+                    sum += x;
+                }
+                else st.push(i);
+            }
+            string m ;
+            while(!st.empty()){
+                m.push_back(st.top());
+                st.pop();
+            }
+            reverse(m.begin(),m.end());
+            for(auto i : m){
+                if(!st.empty() && st.top() == 'b' && i == 'a'){
+                    st.pop();
+                    sum += y;
+                }
+                else st.push(i);
+            }
+        }
+        return sum;
+    }
+};
