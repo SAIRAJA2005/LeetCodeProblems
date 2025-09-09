533a1515c2e085b2778903c89523416c4bfb8a3b
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-10 00:28:53 +0530

    Create 216-CombinationSumIII

---
+++ 216-CombinationSumIII/version/cpp/216-CombinationSumIII_20250910_002853.cpp
@@ -0,0 +1,30 @@
+// Last updated: 10/09/2025, 00:28:53
+class Solution {
+public:
+    void generate(int q, int k, int tar, vector<int>& ans,
+                  vector<vector<int>>& res) {
+        for (auto i : ans) {
+            cout << i << " ";
+        }
+        cout << endl;
+        if (tar < 0)
+            return;
+        if (tar == 0 && ans.size() == k) {
+            // sort(ans.begin(),ans.end());
+            res.push_back(ans);
+            return;
+        }
+        if (q > 9)
+            return;
+        ans.push_back(q);
+        generate(q + 1, k, tar - q, ans, res);
+        ans.pop_back();
+        generate(q + 1, k, tar, ans, res);
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> res;
+        vector<int> ans;
+        generate(1, k, n, ans, res);
+        return res;
+    }
+};
