9363eabfa59b006f5bd49b1adc57b085dc786199
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-10 00:28:55 +0530

    Time: 91 ms (5.37%) | Memory: 9.1 MB (30.04%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    void generate(int q , int k , int tar, vector<int>&ans, vector<vector<int>>&res){
-        for(auto i : ans){
+    void generate(int q, int k, int tar, vector<int>& ans,
+                  vector<vector<int>>& res) {
+        for (auto i : ans) {
             cout << i << " ";
         }
         cout << endl;
-        if(tar < 0) return;
-        if(tar == 0 && ans.size() == k){
-            //sort(ans.begin(),ans.end());
+        if (tar < 0)
+            return;
+        if (tar == 0 && ans.size() == k) {
+            // sort(ans.begin(),ans.end());
             res.push_back(ans);
             return;
         }
-        if(q > 9) return;
+        if (q > 9)
+            return;
         ans.push_back(q);
-        generate(q+1,k,tar-q,ans,res);
+        generate(q + 1, k, tar - q, ans, res);
         ans.pop_back();
-        generate(q+1,k,tar,ans,res);
+        generate(q + 1, k, tar, ans, res);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>>res;
-        vector<int>ans;
-        generate(1,k,n,ans,res);
-        //vector<vector<int>>result;
-        //for(auto i : res) result.push_back(i);
+        vector<vector<int>> res;
+        vector<int> ans;
+        generate(1, k, n, ans, res);
         return res;
     }
 };
