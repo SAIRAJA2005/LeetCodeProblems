49d2775851c61228cb6fe46017bbb99a5396d621
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-09-25 23:25:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (22.22%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    int findTriangle(int i , int j , vector<vector<int>>&triangle,vector<vector<int>>&dp){
-        if(i == triangle.size() - 1) return triangle[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        int one = triangle[i][j] + findTriangle(i + 1 , j , triangle ,dp);
-        int two = triangle[i][j] + findTriangle(i + 1 , j + 1 , triangle , dp);
-        return dp[i][j] = min(one , two);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>>dp (triangle.size(),vector<int>(triangle.size() , -1));
-        return findTriangle(0,0,triangle,dp);
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = triangle[n - 1][i];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                int down = triangle[i][j] + dp[i + 1][j];
+                int downRight = triangle[i][j] + dp[i + 1][j + 1];
+                dp[i][j] = min(down, downRight);
+            }
+        }
+        return dp[0][0];
     }
 };
