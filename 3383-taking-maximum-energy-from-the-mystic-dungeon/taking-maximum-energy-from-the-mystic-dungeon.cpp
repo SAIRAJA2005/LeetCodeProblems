7d325a9f61ed763fcc7ca5135ca3a95508fdf0a1
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-10-10 10:57:21 +0530

    Time: 159 ms (12.32%) | Memory: 145.5 MB (77.46%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -5,8 +5,7 @@ public:
         for(int i = energy.size() - 1 ; i >= 0 ; i--){
             if(i - k >= 0){
                 energy[i - k] += energy[i];
-                cout << energy[i] << " " << energy[i - k];
-                mx = max(mx, energy[i-k]);
+                //mx = max(mx, energy[i-k]);
             }
             mx = max(mx, energy[i]);
             
@@ -14,6 +13,3 @@ public:
         return mx;
     }
 };
-
-// 5 2 -10 -5 1         // k = 3
-// 
