952417419367689fa328756e98744754af85d433
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-12-04 22:56:10 +0530

    Time: 42 ms (70.15%) | Memory: 134.1 MB (92.60%) - LeetSync

---
+++ 4141-count-elements-with-at-least-k-greater-values/count-elements-with-at-least-k-greater-values.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countElements(int[] nums, int k) {
+        if(k == 0) return nums.length;
+        Arrays.sort(nums);
+        int n = nums.length;
+        int t = nums[n - k];
+        int ans = 0;
+        for(int x : nums){
+            if(x < t) ans++;
+        }
+        return ans;
+    }
+}
