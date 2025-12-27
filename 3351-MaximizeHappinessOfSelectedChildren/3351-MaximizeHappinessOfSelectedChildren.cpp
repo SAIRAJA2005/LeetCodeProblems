ca9a8d812285d410934ac212e1b6e950d1a25204
SAIRAJA2005 <119869174+SAIRAJA2005@users.noreply.github.com>
2025-12-27 21:42:54 +0530

    Create 3351-MaximizeHappinessOfSelectedChildren

---
+++ 3351-MaximizeHappinessOfSelectedChildren/3351-MaximizeHappinessOfSelectedChildren.cpp
@@ -0,0 +1,19 @@
+// Last updated: 27/12/2025, 21:42:51
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end());
+
+        if(k == 1) return happiness[happiness.size() - 1];
+        long long total = 0;
+        int n = happiness.size() - 1;
+        for(int i = n ; i >= 0 ; i--){
+            k--;
+            int cal = happiness[i] - (n - i); 
+            if(cal < 0 || k < 0) break;
+            total += cal;
+        }
+
+        return total;
+    }
+};
